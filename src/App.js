63d6c8a397a7fbbb146605ce3f89bb60012419ee
Swapnil <rupakheteeswapnil@gmail.com>
2023-11-05 22:05:48 +0545

    aja ko last

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './index.css';
+import React,{useState} from 'react';
 const api={
   key:"eaa04b891f93d9b9a2d5085b082ac95a",
   base:"http://api.openweathermap.org/data/2.5/"
  }
 
 function App() {
+  const[query,setQuery] =useState("");
+  const[weather,setWeather] = useState({});
+
+  const search =(e)=>{
+    if(e.key==="Enter")
+    {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res=>res.json())
+        .then(result=>{
+          setQuery('');
+          setWeather (result)
+        })
+    }
+  }
+
+
   const dateBuilder=(d)=>{
     let months=["Jan", "Feb", "Mar", "Apr", "May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
     let days=["Sun", "Mon", "Tue","Wed", "Thu", "Fri", "Sat"];
@@ -23,11 +40,15 @@ function App() {
           <input type="text"
                  className='search-bar'
                  placeholder='Search'
+                 onChange={e=>setQuery(e.target.value)}
+                 value={query}
+                 onKeyPress={search}
                  />
         </div>
+        
         <div className='location-box'>
           <div className='location'>
-            Kathmandu,Nepal
+            {weather.name},{weather.sys.country}
             <div className='date'>
               {dateBuilder(new Date())}
 
